7456c062112fe5f0251dbaa402c251aff8516ced
tianyue1994 <tianyue1994@gmail.com>
2018-10-05 14:40:24 -0400

    10051440

--- app.py
+++ app.py
@@ -3,10 +3,20 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'I<3ECE1779'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost:3306/pythonflask'
 Bootstrap(app)
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(15), unique = True)
+    email = db.Column(db.String(50))
+    password = db.Column(db.String(80))
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
@@ -27,18 +37,29 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
-        return '<h1>'+form.username.data+' '+form.password.data+'</h1>'
 
-    return render_template("login.html", form=form)
+        return render_template("login.html", form=form)
 
 @app.route('/signup',methods=['GET','POST'])
 def signup():
     form = RegisterForm()
+
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return 'New User Created'
+
     return render_template("signup.html", form = form)
 
 @app.route('/dashboard')
 def dashboard():
     return render_template("dashboard.html")
+
+@app.route('/test')
+def test():
+    return render_template("test.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
