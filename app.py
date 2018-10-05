d1b3adb8675d4d3bfc2a52d1bdc2f0522ded4ddf
tianyue1994 <tianyue1994@gmail.com>
2018-10-05 12:08:19 -0400

    10051208

--- app.py
+++ app.py
@@ -5,19 +5,36 @@ from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'I<3ECE1779'
 Bootstrap(app)
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/login')
+@app.route('/login',methods=['GET','POST'])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
 
-@app.route('/signup')
+    if form.validate_on_submit():
+        return '<h1>'+form.username.data+' '+form.password.data+'</h1>'
+
+    return render_template("login.html", form=form)
+
+@app.route('/signup',methods=['GET','POST'])
 def signup():
-    return render_template("signup.html")
+    form = RegisterForm()
+    return render_template("signup.html", form = form)
 
 @app.route('/dashboard')
 def dashboard():
